0c565078931761f973309d858a9c154c3f18a42b
vapon <valentinponochevniy@gmail.com>
2016-04-17 16:46:27 +0300

    Fixed index page.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,36 @@
 const path = require('path')
 
+const merge = require('webpack-merge')
+
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
     app: path.join(__dirname, "app"),
     build: path.join(__dirname, "build")
 };
 
-module.exports = {
+const common = {
     entry: {
         app: PATHS.app
     },  
    output: {
        path: PATHS.build,
        filename: 'bundle.js'
+   },
+   module: {
+       loaders: [
+         {
+            test: /\.css$/,
+            loaders: ['style', 'css'],
+            include: PATHS.app
+        }
+      ]
    }
+ }
+ 
+ if(TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {});
+ }
     
-}
+ if(TARGET === 'build') {
+    module.exports = merge(common, {});
+ }
